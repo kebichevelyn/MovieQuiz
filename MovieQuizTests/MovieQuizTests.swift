908fedb666c9b652f8495b2315e92c35fc24783e
kebichevelyn <KebichEvelina@yandex.com>
2025-07-24 10:46:11 +0300

    1 урок 7 спринта

---
+++ MovieQuizTests/MovieQuizTests.swift
@@ -0,0 +1,16 @@
+//
+//  MovieQuizTests.swift
+//  MovieQuizTests
+//
+//  Created by Evelina Kebich on 23.07.25.
+//
+
+import Testing
+
+struct MovieQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
